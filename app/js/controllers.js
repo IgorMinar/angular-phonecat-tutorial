3455e04e78908a89ea4c831e416cc8d8a451a889
Igor Minar <iiminar@gmail.com>
2011-03-05 22:42:31 -0800

    step-5 XHR and dependency injection
    - Replaced the in-memory dataset with data loaded from the server (in
      the form of static phone.json file to make this tutorial backend
      agnostic)
      - The json file is loaded using the [$http] service
    - Demonstrate the use of [services][service] and [dependency injection][DI]
      - The [$http] is injected into the controller through [dependency injection][DI]

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,12 @@
 'use strict';
 /* App Controllers */
 
-function PhoneListCtrl($scope) {
-  $scope.phones = [
-    {"name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S.",
-     "age": 0},
-    {"name": "Motorola XOOM™ with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet.",
-     "age": 1},
-    {"name": "MOTOROLA XOOM™",
-     "snippet": "The Next, Next Generation tablet.",
-     "age": 2}
-  ];
+var PhoneListCtrl = function PhoneListCtrl($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
 
   $scope.orderProp = 'age';
 }
+
+//PhoneListCtrl.$inject = ['$scope', '$scope'];
